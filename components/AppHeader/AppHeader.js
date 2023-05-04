d6c17d41b9eec44c8bb9fb4f99ff6507370940cd
SaiMedha-2000030256 <2000030256@kluniversity.in>
2023-05-04 16:16:07 +0530

    pushing

---
+++ components/AppHeader/AppHeader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './AppHeader.css';
+
+const AppHeader = () => (
+    <header className="app-header">
+        <h1 className="app-title">.docx to pdf converter</h1>
+    </header>
+);
+
+export default AppHeader;
